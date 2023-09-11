2b337d103cb6795a24727f76df951457d88379d2
Toy Santana António <toysan3@hotmail.com>
2023-09-11 11:41:56 +0200

    Update
    - error fix

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,38 +1,19 @@
 import { galleryItems } from "./gallery-items.js";
 
-document.addEventListener("DOMContentLoaded", function () {
-  const galleryContainer = document.querySelector(".gallery");
+const gallery = document.querySelector(".gallery");
 
-  galleryItems.forEach((item) => {
-    const galleryItem = document.createElement("li");
-    galleryItem.classList.add("gallery__item");
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+    </a>
+  `;
+};
 
-    const galleryLink = document.createElement("a");
-    galleryLink.classList.add("gallery__link");
-    galleryLink.href = item.original;
+const galleryMarkup = galleryItems.map(createGalleryItem).join("");
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
 
-    const galleryImage = document.createElement("img");
-    galleryImage.classList.add("gallery__image");
-    galleryImage.src = item.preview;
-    galleryImage.alt = item.description;
-    galleryImage.setAttribute("data-source", item.original);
-    galleryImage.setAttribute("title", item.description);
-
-    galleryLink.appendChild(galleryImage);
-    galleryItem.appendChild(galleryLink);
-
-    galleryContainer.appendChild(galleryItem);
-  });
-
-  const lightbox = new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionDelay: 250,
-  });
-
-  lightbox.on("shown.simplelightbox", (e) => {
-    const captionElement = document.createElement("div");
-    captionElement.classList.add("lightbox-caption");
-    captionElement.innerText = e.opts.captions[e.index];
-    e.modal.appendChild(captionElement);
-  });
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
 });
